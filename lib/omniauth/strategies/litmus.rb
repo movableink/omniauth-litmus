dbf2ea64132735c85b091ed3a17985333eff26a9
Rahim Packir Saibo <i.am@rah.im>
2016-06-17 16:19:39 +0100

    Add support for user metadata omniauth auth hash population

--- lib/omniauth/strategies/litmus.rb
+++ lib/omniauth/strategies/litmus.rb
@@ -1,4 +1,4 @@
-require 'omniauth-oauth2'
+require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
@@ -7,6 +7,22 @@ module OmniAuth
       option :client_options, {
         site: ENV["LITMUS_OAUTH_HOST"] || "https://litmus.com"
       }
+
+      uid { raw_info["id"].to_s }
+      info do
+        {
+          "name"       => raw_info["full_name"],
+          "first_name" => raw_info["first_name"],
+          "last_name"  => raw_info["last_name"]
+        }
+      end
+
+      def raw_info
+        access_token.options[:mode] = :query
+        @raw_info ||= access_token.
+                        get("https://api.litmus.com/v2/users/current").
+                        parsed["user"]
+      end
     end
   end
 end
