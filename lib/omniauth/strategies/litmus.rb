9fba5f1b3506f4c61a2456218cad10a5b92ecc93
Rahim Packir Saibo <i.am@rah.im>
2016-06-17 14:44:48 +0100

    Correct strategy class name

--- lib/omniauth/strategies/litmus.rb
+++ lib/omniauth/strategies/litmus.rb
@@ -2,7 +2,7 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class SomeSite < OmniAuth::Strategies::OAuth2
+    class Litmus < OmniAuth::Strategies::OAuth2
       option :name, "litmus"
       option :client_options, {
         site: ENV["LITMUS_OAUTH_PROVIDER"] || "https://litmus.com"
